95367a19a0085e05436c7b1bb5d3a4a213475f7c
stambolievv <deyan.stamboliev@gmail.com>
2021-12-02 18:07:09 +0200

    add myBooks view

--- Advanced JS/Applications JS/Practical Exam Preparation/02-Books Library/src/views/myBooksPage/myBooksView.js
+++ Advanced JS/Applications JS/Practical Exam Preparation/02-Books Library/src/views/myBooksPage/myBooksView.js
@@ -2,6 +2,28 @@ import { html, until } from '../../lib/lib.js';
 import { spinner } from '../../common/spinner.js';
 
 export const template = (myProfilePromise) => html`
+    <!-- My Books Page ( Only for logged-in users ) -->
+    <section id="my-books-page" class="my-books">
+        <h1>My Books</h1>
+        <!-- Display ul: with list-items for every user's books (if any) -->
+        <ul class="my-books-list">
+            <li class="otherBooks">
+                <h3>Outlander</h3>
+                <p>Type: Other</p>
+                <p class="img"><img src="/images/book2.png"></p>
+                <a class="button" href="#">Details</a>
+            </li>
+            <li class="otherBooks">
+                <h3>A Court of Thorns and Roses</h3>
+                <p>Type: Fiction</p>
+                <p class="img"><img src="/images/book1.png"></p>
+                <a class="button" href="#">Details</a>
+            </li>
+        </ul>
+    
+        <!-- Display paragraph: If the user doesn't have his own books  -->
+        <p class="no-books">No books in database!</p>
+    </section>
     <!-- Profile Page ( Only for logged users ) -->
     <section id="user-profile-page" class="user-profile">
         ${until(loadData(myProfilePromise), spinner())}

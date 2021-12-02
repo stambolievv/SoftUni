7daead4ebaff102380bafc0544f695898ec758ef
stambolievv <deyan.stamboliev@gmail.com>
2021-12-02 20:27:19 +0200

    add searchPage

--- Advanced JS/Applications JS/Practical Exam Preparation/02-Books Library/src/app.js
+++ Advanced JS/Applications JS/Practical Exam Preparation/02-Books Library/src/app.js
@@ -7,6 +7,7 @@ import { createPage } from './views/createPage/createController.js';
 import { editPage } from './views/editPage/editController.js';
 import { loginPage } from './views/loginPage/loginController.js';
 import { registerPage } from './views/registerPage/registerController.js';
+import { searchPage } from './views/searchPage/searchController.js';
 
 page(decorateContext);
 page('/home', homePage);
@@ -16,6 +17,7 @@ page('/edit/:id', editPage);
 page('/details/:id', detailsPage);
 page('/users/login', loginPage);
 page('/users/register', registerPage);
+page('/search', searchPage);
 
 page.redirect('/', '/home');
 page.start();

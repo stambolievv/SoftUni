178c22f0015b72c13fbeb62a78a7e2bd0213cab0
Deyan Stamboliev <93904336+stambolievv@users.noreply.github.com>
2022-01-14 21:53:42 +0200

    commit

--- Fundamentals JS/01-Basic Syntax, Conditional Statements and Loops/Exercise/07-triangleOfNumbers.js
+++ Fundamentals JS/01-Basic Syntax, Conditional Statements and Loops/Exercise/07-triangleOfNumbers.js
@@ -1,10 +1,16 @@
+// function triangleOfNumbers(step) {
+//     for (let i = 1; i <= step; i++) {
+//         let space = '';
+//         for (let j = 1; j <= i; j++) {
+//             space += `${i} `;
+//         }
+//         console.log(space);
+//     }
+// }
+
 function triangleOfNumbers(step) {
     for (let i = 1; i <= step; i++) {
-        let space = '';
-        for (let j = 1; j <= i; j++) {
-            space += `${i} `;
-        }
-        console.log(space);
+        console.log(`${i} `.repeat(i);
     }
 }
 triangleOfNumbers(3);

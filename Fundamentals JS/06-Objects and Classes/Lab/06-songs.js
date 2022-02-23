9fe039dbbabf5b0320fbec1559874da52590cd52
stambolievv <deyan.stamboliev@gmail.com>
2022-02-23 23:19:39 +0200

    commit

--- Fundamentals JS/06-Objects and Classes/Lab/06-songs.js
+++ Fundamentals JS/06-Objects and Classes/Lab/06-songs.js
@@ -1,34 +1,30 @@
 function songs(arr) {
-    class Song {
-        constructor(typeList, songName, timeOfSong) {
-            this.typeList = typeList;
-            this.name = songName;
-            this.time = timeOfSong;
-        }
+
+  class Song {
+    constructor(type, name, time) {
+      this.type = type;
+      this.name = name;
+      this.time = time;
     }
+  }
 
-    let numberOfSongs = arr.shift();
-    let commandType = arr.pop();
+  const numberOfSongs = arr.shift();
+  const commandType = arr.pop();
 
-    let listOfSongs = [];
+  const listOfSongs = [];
 
-    for (const iterator of arr) {
-        let [type, name, time] = iterator.split('_');
-        let currentSong = new Song(type, name, time);
-        listOfSongs.push(currentSong);
+  for (const song of arr) {
+    const [type, name, time] = song.split('_');
+    listOfSongs.push(new Song(type, name, time));
+  }
 
-        if (commandType === 'all') {
-            listOfSongs.forEach((s) => {
-                console.log(s.name);
-            });
-        } else {
-            let filtered = listOfSongs.filter((i) => i.typeList === commandType);
-            filtered.forEach((s) => {
-                console.log(s.name);
-            });
-        }
-    }
+  if (commandType == 'all') {
+    listOfSongs.forEach(s => console.log(s.name));
+  } else {
+    listOfSongs.filter(i => i.type == commandType).forEach(s => console.log(s.name));
+  }
 }
+
 songs([3, 'favourite_DownTown_3:14', 'favourite_Kiss_4:16', 'favourite_Smooth Criminal_4:01', 'favourite']);
 songs([4, 'favourite_DownTown_3:14', 'listenLater_Andalouse_3:24', 'favourite_In To The Night_3:58', 'favourite_Live It Up_3:48', 'listenLater']);
 songs([2, 'like_Replay_3:15', 'ban_Photoshop_3:48', 'all']);
